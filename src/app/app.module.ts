90dd063fb1ea1b6c57dff1016da564a78e56d813
HUNGDT <hk2t.thaihung@gmail.com>
2019-02-17 17:07:00 +0700

    todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BlogComponent } from './blog/blog.component';
-import { BlogEditComponent } from './blog-edit/blog-edit.component';
-import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BlogComponent} from './blog/blog.component';
+import {BlogEditComponent} from './blog-edit/blog-edit.component';
+import {BlogDetailComponent} from './blog-detail/blog-detail.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {TodoComponent} from './todo/todo.component';
+import {TokenInterceptor} from './token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
     BlogEditComponent,
-    BlogDetailComponent
+    BlogDetailComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,12 @@ import {HttpClientModule} from '@angular/common/http';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit} from '@angular/core';
+import {ITodo} from '../itodo';
+import {FormControl} from '@angular/forms';
+import {TodoService} from '../todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+  todoList: ITodo[] = [];
+  todoControl = new FormControl();
+
+  constructor(private todoService: TodoService) {
+  }
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(
+      next => {
+        this.todoList = next;
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        console.log('complete');
+      }
+    );
+  }
+
+  toggleTodo(i) {
+    const todo = this.todoList[i];
+    const todoData = {...todo, completed: !todo.completed};
+    this.todoService.updateTodo(todoData).subscribe(
+      next => {
+        this.todoList[i].completed = next.completed;
+      }
+    );
+  }
+
+  addTodo() {
+    const todo: Partial<ITodo> = {
+      title: this.todoControl.value,
+      completed: false
+    };
+    this.todoService.createTodo(todo).subscribe(
+      next => {
+        this.todoList.unshift(next);
+        this.todoControl.setValue('');
+      }
+    );
+  }
+
+  deleteTodo(i) {
+    const todo = this.todoList[i];
+    this.todoService.deleteTodo(todo.id).subscribe(
+      next => {
+        this.todoList = this.todoList.filter(
+          t => t.id !== todo.id
+        );
+      }
+    );
+  }
+
+}
